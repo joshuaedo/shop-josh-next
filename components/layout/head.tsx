78a372b9d8aabe93d80a0c6ef78bddd8801b10fd
Joshua Edo <joshua.edo01@gmail.com>
2024-05-20 18:06:32 +0100

    refactor: :test_tube: test page-head without apphead

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -16,6 +16,7 @@ const AppHead = () => {
       <meta property='og:logo' content='/double-legged-logo.png' />
       <meta name='url' content={url} />
       <meta name='identifier-URL' content={url} />
+      <meta name='robots' content='index,follow' />
       <meta
         name='robots'
         content='max-snippet:155, max-image-preview:standard'
@@ -38,13 +39,30 @@ const PageHead = ({ title, description, image }: HeadProps) => {
       <meta name={description} />
       <meta name='title' content={title} />
       <meta name='pagename' content={title} />
-      <meta name='robots' content='index,follow' />
       <meta property='og:title' content={title} />
       <meta property='og:description' content={description} />
       <meta property='og:image:url' content={image} />
       <meta name='twitter:title' content={title} />
       <meta name='twitter:description' content={description} />
       <meta name='twitter:image' content={image} />
+
+      {/* testing without Apphead */}
+      <link rel='icon' href='/double-legged-logo.png' />
+      <meta property='og:logo' content='/double-legged-logo.png' />
+      <meta name='url' content={url} />
+      <meta name='identifier-URL' content={url} />
+      <meta name='robots' content='index,follow' />
+      <meta
+        name='robots'
+        content='max-snippet:155, max-image-preview:standard'
+      />
+      <meta property='og:url' content={url} />
+      <meta property='og:site_name' content={siteName} />
+      {/* <meta property='og:image:width' content='400' />
+      <meta property='og:image:height' content='250' /> */}
+      <meta property='og:type' content='website' />
+      <meta name='twitter:card' content='summary' />
+      <meta name='twitter:creator' content={creator.name} />
     </NextHead>
   );
 };
