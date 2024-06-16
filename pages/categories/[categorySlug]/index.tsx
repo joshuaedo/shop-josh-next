16c27ab681183323357628ef318365bd05e59fef
Joshua Edo <joshua.edo01@gmail.com>
2024-06-16 01:42:02 +0100

    feat: :sparkles: generate dynamic grid images for category pages
    - Implemented the `generateGridImage` function to create a grid of images. - Enhanced image quality by resizing images to 200x200 pixels. - Dynamically adjust grid layout based on the number of images. - Appended a unique identifier to the generated image URL to handle caching issues. - Updated `getServerSideProps` to use the new `generateGridImage` function. - Integrated the new grid image into the `CategoryPage` component.

--- pages/categories/[categorySlug]/index.tsx
+++ pages/categories/[categorySlug]/index.tsx
@@ -11,26 +11,33 @@ import { GetServerSideProps } from 'next';
 import { getCategoryBySlug } from '@/features/categories/lib/queries';
 import { ExtendedCategory } from '@/features/categories/types/extensions';
 import { Suspense } from 'react';
+// import { ProductImage } from '@/features/products/components/product-image';
+import { generateGridImage } from '@/features/categories/lib/utils';
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
 interface CategoryPageProps {
   category: ExtendedCategory;
+  gridImageUrl: string | null;
 }
-
-const CategoryPage = ({ category }: CategoryPageProps) => {
+const CategoryPage = ({ category, gridImageUrl }: CategoryPageProps) => {
   const { description } = siteConfig;
   return (
     <Suspense fallback={<PageLoader />}>
       <PageHead
         title={category?.name}
         description={description}
-        image={category?.images && category?.images[0]?.url}
+        image={gridImageUrl ?? ''}
       />
       <Page>
         <Header title={category?.name} />
         <ProductGrid>
+          {/* <ProductImage
+            type='grid-item'
+            alt={category?.name}
+            image={{ url: gridImageUrl ?? '' }}
+          /> */}
           {category?.products &&
             category?.products?.map((product) => (
               <ProductGridItem key={product?.id} product={product} />
@@ -52,9 +59,14 @@ export const getServerSideProps: GetServerSideProps<
     slug: typeof categorySlug === 'string' ? categorySlug : undefined,
   });
 
+  const imageUrls = category?.images?.map((image) => image.url) || [];
+  const gridImageUrl =
+    imageUrls.length > 0 ? await generateGridImage(imageUrls) : null;
+
   return {
     props: {
       category,
+      gridImageUrl,
     },
   };
 };
