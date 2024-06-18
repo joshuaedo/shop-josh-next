9bc246cc1f4fcdf86a96539c82ed2c104bf2a7bb
Joshua Edo <joshua.edo01@gmail.com>
2024-06-19 00:41:02 +0100

    fix: :construction: retire grid image generator due to storage unavailability

--- features/categories/lib/utils.ts
+++ features/categories/lib/utils.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import sharp from 'sharp';
 import fs from 'fs';
 
-// Helper function to shuffle an array and pick the first n elements
 const getRandomImages = (array: string[], num: number) => {
   const shuffled = array.sort(() => 0.5 - Math.random());
   return shuffled.slice(0, num);
@@ -12,78 +11,77 @@ const generateGridImage = async (imageUrls: string[]): Promise<string> => {
   let selectedImages: string[] = [];
   let columns: number, rows: number;
 
-  try {
-    if (imageUrls.length > 9) {
-      selectedImages = getRandomImages(imageUrls, 9);
-      columns = 3;
-      rows = 3;
-    } else if (imageUrls.length === 9) {
-      selectedImages = imageUrls;
-      columns = 3;
-      rows = 3;
-    } else if (imageUrls.length > 6 && imageUrls.length < 9) {
-      selectedImages = getRandomImages(imageUrls, 6);
-      columns = 3;
-      rows = 2;
-    } else if (imageUrls.length === 6) {
-      selectedImages = imageUrls;
-      columns = 3;
-      rows = 2;
-    } else if (imageUrls.length > 4 && imageUrls.length < 6) {
-      selectedImages = getRandomImages(imageUrls, 4);
-      columns = 2;
-      rows = 2;
-    } else if (imageUrls.length === 4) {
-      selectedImages = imageUrls;
-      columns = 2;
-      rows = 2;
-    } else {
-      selectedImages = imageUrls; // Handle cases with fewer than 4 images (can be extended as needed)
-      columns = Math.min(2, imageUrls.length);
-      rows = Math.ceil(imageUrls.length / columns);
-    }
-
-    const images = await Promise.all(
-      selectedImages.map(async (url, index) => {
-        const response = await fetch(url);
-        if (!response.ok) {
-          throw new Error(`Failed to fetch image at ${url}`);
-        }
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-        const resizedImage = await sharp(buffer).resize(200, 200).toBuffer();
-        return {
-          input: resizedImage,
-          top: Math.floor(index / columns) * 200, // Adjust based on grid layout
-          left: (index % columns) * 200, // Adjust based on grid layout
-        };
-      })
-    );
-
-    const gridWidth = columns * 200;
-    const gridHeight = rows * 200;
-
-    const gridBuffer = await sharp({
-      create: {
-        width: gridWidth,
-        height: gridHeight,
-        channels: 4,
-        background: { r: 255, g: 255, b: 255, alpha: 0 },
-      },
-    })
-      .composite(images)
-      .png()
-      .toBuffer();
-
-    const uniqueId = Date.now();
-    const outputPath = path.join('/tmp', `grid-image-${uniqueId}.png`);
-    fs.writeFileSync(outputPath, gridBuffer);
-
-    return outputPath; // Return the absolute path for the temporary file
-  } catch (error) {
-    console.error('Error generating grid image:', error);
-    throw new Error('Failed to generate grid image');
+  if (imageUrls.length > 9) {
+    selectedImages = getRandomImages(imageUrls, 9);
+    columns = 3;
+    rows = 3;
+  } else if (imageUrls.length === 9) {
+    selectedImages = imageUrls;
+    columns = 3;
+    rows = 3;
+  } else if (imageUrls.length > 6 && imageUrls.length < 9) {
+    selectedImages = getRandomImages(imageUrls, 6);
+    columns = 3;
+    rows = 2;
+  } else if (imageUrls.length === 6) {
+    selectedImages = imageUrls;
+    columns = 3;
+    rows = 2;
+  } else if (imageUrls.length > 4 && imageUrls.length < 6) {
+    selectedImages = getRandomImages(imageUrls, 4);
+    columns = 2;
+    rows = 2;
+  } else if (imageUrls.length === 4) {
+    selectedImages = imageUrls;
+    columns = 2;
+    rows = 2;
+  } else {
+    selectedImages = imageUrls;
+    columns = Math.min(2, imageUrls.length);
+    rows = Math.ceil(imageUrls.length / columns);
   }
+
+  const images = await Promise.all(
+    selectedImages.map(async (url, index) => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Failed to fetch image at ${url}`);
+      }
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      const resizedImage = await sharp(buffer).resize(200, 200).toBuffer();
+      return {
+        input: resizedImage,
+        top: Math.floor(index / columns) * 200,
+        left: (index % columns) * 200,
+      };
+    })
+  );
+
+  const gridWidth = columns * 200;
+  const gridHeight = rows * 200;
+
+  const gridBuffer = await sharp({
+    create: {
+      width: gridWidth,
+      height: gridHeight,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 0 },
+    },
+  })
+    .composite(images)
+    .png()
+    .toBuffer();
+
+  const uniqueId = Date.now();
+  const outputPath = path.join(
+    process.cwd(),
+    'public',
+    `grid-image-${uniqueId}.png`
+  );
+  fs.writeFileSync(outputPath, gridBuffer);
+
+  return `/grid-image-${uniqueId}.png`;
 };
 
 export { generateGridImage };
--- pages/categories/[categorySlug]/index.tsx
+++ pages/categories/[categorySlug]/index.tsx
@@ -11,33 +11,28 @@ import { GetServerSideProps } from 'next';
 import { getCategoryBySlug } from '@/features/categories/lib/queries';
 import { ExtendedCategory } from '@/features/categories/types/extensions';
 import { Suspense } from 'react';
-// import { ProductImage } from '@/features/products/components/product-image';
-import { generateGridImage } from '@/features/categories/lib/utils';
+import { shuffle } from 'lodash';
 
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 
 interface CategoryPageProps {
   category: ExtendedCategory;
-  gridImageUrl: string | null;
 }
-const CategoryPage = ({ category, gridImageUrl }: CategoryPageProps) => {
+const CategoryPage = ({ category }: CategoryPageProps) => {
   const { description } = siteConfig;
+  const imageUrls = category?.images?.map((image) => image.url) || [];
+  const shuffledImages = shuffle(imageUrls);
   return (
     <Suspense fallback={<PageLoader />}>
       <PageHead
         title={category?.name}
         description={description}
-        image={gridImageUrl ?? ''}
+        image={shuffledImages[0]}
       />
       <Page>
         <Header title={category?.name} />
         <ProductGrid>
-          {/* <ProductImage
-            type='grid-item'
-            alt={category?.name}
-            image={{ url: gridImageUrl ?? '' }}
-          /> */}
           {category?.products &&
             category?.products?.map((product) => (
               <ProductGridItem key={product?.id} product={product} />
@@ -59,14 +54,9 @@ export const getServerSideProps: GetServerSideProps<
     slug: typeof categorySlug === 'string' ? categorySlug : undefined,
   });
 
-  const imageUrls = category?.images?.map((image) => image.url) || [];
-  const gridImageUrl =
-    imageUrls.length > 0 ? await generateGridImage(imageUrls) : null;
-
   return {
     props: {
       category,
-      gridImageUrl,
     },
   };
 };
